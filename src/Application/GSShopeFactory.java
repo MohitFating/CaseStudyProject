56b49dae6a01f01d6c8183b8a37a1e1fafcddb36
Mohit Fating <mohitfating007@gmail.com>
2024-02-26 23:02:51 +0530

    update file

--- src/Application/GSShopeFactory.java
+++ src/Application/GSShopeFactory.java
@@ -1,5 +1,19 @@
 package Application;
+import Framework.NormalAcc;
+import Framework.PrimeAcc;
+import Framework.ShopFactory;
 
-public class GSShopeFactory {
+public class GSShopeFactory extends ShopFactory
+{
 
+	public GSPrimeAcc getNewPrimeAccount(int accNo, String accNm,float charges, boolean isPrime) {
+		GSPrimeAcc gsprime = new GSPrimeAcc(accNo, accNm, charges, isPrime);
+		return gsprime;
+		
+	}
+	public GSNormalAcc getNewNormalAccount(int accNo, String accNm,float charges, float deliveryCharge) {
+		GSNormalAcc gsnormal= new GSNormalAcc(accNo,  accNm, charges,  deliveryCharge);
+		return gsnormal;
+		
+	}
 }
